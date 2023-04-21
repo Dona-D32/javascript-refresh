387a312a6e6d454bb5efb63d3342b5761f77fac2
Dona_D32 <dodadi@hotmail.ca>
2023-04-20 22:05:57 -0400

    Ajoutez un fichier JavaScript nommé array-methods.js

---
+++ exercice_1/1.js
@@ -0,0 +1,13 @@
+/*
+	Écrivez une fonction qui prend en entrée une température en degrés Celsius et la convertit en degrés Fahrenheit.
+
+	°F = (°C × 9/5) + 32
+*/
+
+const celsiusToFahrenheit = (celsius) => {
+	const fahrenheit = (celsius * 9/5) + 32;
+	return fahrenheit;
+  }
+  
+
+  
